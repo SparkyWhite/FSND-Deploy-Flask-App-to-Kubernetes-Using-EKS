d58d600aafebe8e0c940b3309f8e1c5315cbb6da
randyrichards <randy.richards@columbiacu.org>
2020-04-22 10:21:51 -0700

    Now?

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy, sir yes sir")
+    return jsonify("Healthy, nope")
 
 
 @APP.route('/auth', methods=['POST'])
